aba9bf95b1903b171cb1baddbd2d177e06a1c934
Douglas S. Leonard <dleonard.dev@gmail.com>
2026-05-19 09:01:59 +0900

    chore: doc update
    Includes adding DLG4Units to main docs

--- MakeDocs.sh
+++ MakeDocs.sh
@@ -1,23 +1,43 @@
-#two-pass to get dot file path translation
-# replace doctoc with doxygen md TOC marker
-Process_md(){
-  file =$1
-sed -i '/<!-- START doctoc generated TOC please keep comment here to allow auto update -->/,/<!-- END doctoc generated TOC please keep comment here to allow auto update -->/{
-  /<!-- START doctoc generated TOC please keep comment here to allow auto update -->/{
-    s/.*/[TOC]/
-    b
-  }
-  d
-}' file
-rm ./docs/html -rf
-doxygen ./docs/Doxyfile
-doxygen ./docs/Doxyfile
-rm -rf public
-cp -arf ./docs/html public
-#sed -i 's/@ref //g' README.md
-sed -i 's/^\[TOC\]$/<!-- START doctoc generated TOC please keep comment here to allow auto update -->\n<!-- END doctoc generated TOC please keep comment here to allow auto update -->/' file
-doctoc file
+#!/bin/bash
+
+Files=(
+  README.md
+  GeoModules/GeoModulesREADME.md
+)
+
+prepare() {
+# Prepare files for Doxygen (Replace doctoc with [TOC])
+for file in "${Files[@]}"; do
+  sed -i '/<!-- START doctoc generated TOC please keep comment here to allow auto update -->/,/<!-- END doctoc generated TOC please keep comment here to allow auto update -->/{
+    /<!-- START doctoc generated TOC please keep comment here to allow auto update -->/{
+      s/.*/[TOC]/
+      b
+    }
+    d
+  }' "$file"
+done
 }
 
-for file in README.md GeoModulesREADME.md; docs
-Process_md file
+do_doxygen (){
+#  Run Doxygen (Build the documentation once)
+rm -rf ./docs/html
+doxygen ./docs/Doxyfile
+doxygen ./docs/Doxyfile # Second pass for dot graph translations
+
+#  Cleanup and Restore
+rm -rf public
+cp -arf ./docs/html public
+}
+
+restore() {
+for file in "${Files[@]}"; do
+    # Restore the doctoc markers
+    sed -i 's/^\[TOC\]$/<!-- START doctoc generated TOC please keep comment here to allow auto update -->\n<!-- END doctoc generated TOC please keep comment here to allow auto update -->/' "$file"
+    # Update the actual TOC content
+    doctoc "$file"
+done
+}
+
+prepare
+do_doxygen
+restore
